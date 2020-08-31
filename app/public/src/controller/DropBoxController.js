29cf9c22dd6dd5f993d870044964ad4362b2350a
Addson <addson@grupow.com.br>
2020-08-31 14:18:34 -0300

    snackbar

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -4,6 +4,9 @@ class DropBoxController{
     this.btnSendFileEl = document.querySelector('#btn-send-file');
     this.inputFilesEl = document.querySelector('#files');
     this.snackModalEl = document.querySelector('#react-snackbar-root');
+    this.progressBarEl = this.snackModalEl.querySelector('.mc-progress-bar-fg');
+    this.nameFileEl = this.snackModalEl.querySelector('.filename');
+    this.timeleftEl = this.snackModalEl.querySelector('.timeleft');
     this.initEvents();
   }
 
@@ -36,6 +39,12 @@ class DropBoxController{
         ajax.onerror = event=>{
           reject(event);
         }
+
+        ajax.upload.onprogress = event=>{
+          this.uploadProgress(event, file);
+        }
+        this.startUploadTime = Date.now();
+
         let formData = new FormData();
         //nome do campo pode ser qualquer um, aqui no caso foi utilizado 'input-file'
         formData.append('input-file',file);
@@ -44,7 +53,36 @@ class DropBoxController{
       }));
     })
 
-    return Promise.all(promises)
+    return Promise.all(promises);
+  }
+  uploadProgress(event,file){
+    let timeSpent = Date.now() - this.startUploadTime;
+    let loaded = event.loaded;
+    let total = event.total;
+    let porcent = parseInt((loaded/total)*100);
+    let timeleft = ((100 - porcent)*timeSpent) / porcent;
+    
+    this.progressBarEl.style.width= `${porcent}%`;
+
+    this.nameFileEl.innerHTML = file.name;
+    this.timeleftEl.innerHTML = this.formatTime(timeleft);
+  }
+
+  formatTime(duration){
+    let seconds = parseInt((duration/1000) % 60);
+    let minutes = parseInt((duration /(1000 * 60)) % 60) ;
+    let hours = parseInt((duration /(1000 * 60 * 60)) % 24);
+
+    if(hours>0){
+      return `${hours} horas, ${minutes} minutos e ${seconds} segundos`;
+    }
+    if(minutes>0){
+      return `${minutes} minutos e ${seconds} segundos`;
+    }
+    if(seconds >0){
+      return `${seconds} segundos`;
+    }
+    return '';
   }
 
 }
